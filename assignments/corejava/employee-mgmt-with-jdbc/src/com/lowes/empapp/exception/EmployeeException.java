3db3708a4e9e88ce23f200347f86df1d7c31e4e5
jahnavi <jahnavi28@gmail.com>
2022-05-31 08:35:40 +0530

    jdbc

---
+++ assignments/corejava/employee-mgmt-with-jdbc/src/com/lowes/empapp/exception/EmployeeException.java
@@ -0,0 +1,23 @@
+package com.lowes.empapp.exception;
+
+public class EmployeeException extends Exception {
+	
+
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public EmployeeException (String str)
+
+	{
+
+	super(str);
+
+	}
+   
+   
+
+	}
+
+
